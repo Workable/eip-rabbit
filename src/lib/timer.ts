1d101e9874dde175716c9313c91f3f382b60d83b
Nikos Kostoulas <kostoulas@workable.com>
2017-04-21 15:42:37 +0300

    Update rabbit dependency and recreate queue on connection.

--- src/lib/timer.ts
+++ src/lib/timer.ts
@@ -5,6 +5,11 @@ let timerCounter = 0;
 export default class Timer extends TimerInterface {
   constructor(public delays, public rabbit: Rabbit, public name = `eip_${timerCounter++}`) {
     super(delays);
+    this.rabbit.on('connected', () => this.createQueue());
+    this.createQueue();
+  }
+
+  createQueue() {
     this.rabbit.createQueue(this.name, {}, (msg, ack) => {
       const { id, delay, attempt } = JSON.parse(msg.content.toString());
       this.inject(id, attempt, delay);
